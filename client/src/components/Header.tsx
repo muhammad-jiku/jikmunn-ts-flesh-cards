0910cc3ef80167f284741ccd4a57b18a3f07d3dd
muhammad-jiku <muhammadjiku364@gmail.com>
2024-04-23 23:23:25 +0600

    component index file added

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Header.module.css';
 
-export function Header() {
+export default function Header() {
   return (
     <div className={`${styles.header}`}>
       <div className={`${styles.container}`}>
---
+++ client/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Deck } from './Deck';
+export { default as Decks } from './Decks';
+export { default as DeckForm } from './DeckForm';
+export { default as Header } from './Header';
