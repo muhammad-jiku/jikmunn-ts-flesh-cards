eb944bf7e836ff5781fc993eb929c50da67bd951
muhammad-jiku <muhammadjiku364@gmail.com>
2024-04-23 23:03:39 +0600

    decks component and css file added

---
+++ client/src/components/Decks.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { TDeck } from '../types';
+import { deleteDeck, getDecks } from '../api';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Decks.module.css';
+
+export default function Decks() {
+  const [decks, setDecks] = useState<TDeck[]>([]);
+
+  async function handleDeleteDeck(deckId: string) {
+    await deleteDeck(deckId);
+    setDecks(decks.filter((deck) => deck._id !== deckId));
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      const newDecks = await getDecks();
+      setDecks(newDecks);
+    }
+    fetchDecks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Your Decks</h1>
+
+      <ul className={`${styles.decks}`}>
+        {decks.map((deck) => (
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+
+            <Link to={`decks/${deck._id}`}>{deck.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
