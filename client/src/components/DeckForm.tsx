b3c1f9bb5e9e58fa05e27caa04917869358c9e07
muhammad-jiku <muhammadjiku364@gmail.com>
2024-04-23 23:11:25 +0600

    deck form component and styles files added

---
+++ client/src/components/DeckForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { TDeck } from '../types';
+import { createDeck } from '../api';
+import styles from '../styles/DeckForm.module.css';
+
+export default function DeckForm() {
+  const [decks, setDecks] = useState<TDeck[]>([]);
+  const [title, setTitle] = useState('');
+
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault();
+    const deck = await createDeck(title);
+    setDecks([...decks, deck]);
+    setTitle('');
+  }
+
+  return (
+    <div className={`${styles.container}`}>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor='deck-title'>Deck Title</label>
+        <input
+          id='deck-title'
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
+    </div>
+  );
+}
