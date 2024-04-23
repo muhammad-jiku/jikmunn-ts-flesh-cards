538562ca96c059ec14c27ecad7898ca09e6bc223
muhammad-jiku <muhammadjiku364@gmail.com>
2024-04-23 22:17:09 +0600

    component files added

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import styles from '../styles/Header.module.css';
+
+export function Header() {
+  return (
+    <div className={`${styles.header}`}>
+      <div className={`${styles.container}`}>
+        <div>
+          <a href='/'>FlashCardSage</a>
+        </div>
+
+        <div>
+          <a href='/'>Decks</a>
+        </div>
+
+        <div>
+          <a href='/login'>login</a>
+        </div>
+      </div>
+    </div>
+  );
+}
